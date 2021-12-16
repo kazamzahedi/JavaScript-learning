7b459dc20cc47033728b226525ab75fc5b6817c6
kazam zahedi <kazam.zahedi@gmail.com>
2021-12-16 10:58:38 +0100

    Classes: Object blueprints

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
 /**
- * Create a Backpack object.
+ * Create a class for the Backpack object type.
+ * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
  */
+ import Backpack from "./Backpack.js";
 
- const backpack = {
-    name: "Everyday Backpack",
-    volume: 30,
-    color: "grey",
-    pocketNum: 15,
-    strapLength: {
-      left: 26,
-      right: 26,
-    },
-    lidOpen: false,
-    toggleLid: function (lidStatus) {
-      this.lidOpen = lidStatus;
-    },
-    newStrapLength: function (lengthLeft, lengthRight) {
-      this.strapLength.left = lengthLeft;
-      this.strapLength.right = lengthRight;
-    },
-  };
-  
-  console.log("The backpack object:", backpack);
+ const everydayPack = new Backpack(
+   "Everyday Backpack",
+   30,
+   "grey",
+   15,
+   26,
+   26,
+   false
+ );
+ 
+  console.log("The everydayPack object:", everydayPack);
+  console.log("The pocketNum value:", everydayPack.pocketNum);
+  console.log(everydayPack.strapLength.left);
