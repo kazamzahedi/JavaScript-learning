200b27c39578972368c29db23e9ce784c1bb85eb
kazam zahedi <kazam.zahedi@gmail.com>
2022-01-20 09:25:07 +0100

    The arrow function

--- Functions/script.js
+++ Functions/script.js
@@ -1,27 +1,30 @@
 /**
- * Working with functions
+ * A standard function
  * @link https://developer.mozilla.org/en-US/docs/Glossary/Function
  */
 
-// Function declaration:
-function doSomeMath(a, b) {
-    let c = a + b;
-    return c;
-  }
-  
-  // Function expression:
-  const doMoreMath = function (a = 3, b = 2) {
-    let c = a * b;
-    return c;
-  };
-  
-  console.log("Do some math:", doSomeMath(5, 6));
-  console.log("Do more math:", doMoreMath(5, 6));
-  
-  // Immediately Invoked Function Expression (IIFE)
-  // (function () {
-  //   let a = 4;
-  //   let b = 6;
-  //   let c = doSomeMath(a, b);
-  //   console.log(`The sum of a and b is: ${c}`);
-  // })();
+const greenPack = {
+    name: "Frog Pack",
+    color: "green",
+    volume: 8,
+    pocketNum: 3 
+};
+
+const addPack = (currentPack) => {
+    const newArticle = document.createElement("article");
+    newArticle.innerHTML = `
+        <h1>${currentPack.name}</h1>
+        <ul>
+            <li>Volume: ${currentPack.volume}</li>
+            <li>Color: ${currentPack.color}</li>
+            <li>Number of packets: ${currentPack.pocketNum}</li>
+        </ul>
+    `;
+    return newArticle;
+};
+
+const theArticle = addPack(greenPack);
+console.log(theArticle);
+
+const main = document.querySelector("main");
+main.append(addPack(greenPack));
