c25c937c7517cdd395cefb4da8b7e756ca1914b2
kazam zahedi <kazam.zahedi@gmail.com>
2022-01-20 10:40:52 +0100

    arrow function and "this"

--- Functions/script.js
+++ Functions/script.js
@@ -7,24 +7,12 @@ const greenPack = {
     name: "Frog Pack",
     color: "green",
     volume: 8,
-    pocketNum: 3 
+    pocketNum: 3,
+    newVolume: function (volume) {
+        console.log("this.volume in the method:", this.volume);
+        this.volume = volume;
+        console.log("this.volume after update:", this.volume);
+    },
 };
 
-const addPack = (currentPack) => {
-    const newArticle = document.createElement("article");
-    newArticle.innerHTML = `
-        <h1>${currentPack.name}</h1>
-        <ul>
-            <li>Volume: ${currentPack.volume}</li>
-            <li>Color: ${currentPack.color}</li>
-            <li>Number of packets: ${currentPack.pocketNum}</li>
-        </ul>
-    `;
-    return newArticle;
-};
-
-const theArticle = addPack(greenPack);
-console.log(theArticle);
-
-const main = document.querySelector("main");
-main.append(addPack(greenPack));
+console.log(greenPack.newVolume(5));
